20ff07ecf5a8f1889b11bc96f311e37b627c0b81
assafainhoren <Assafain@gmail.com>
2022-04-12 08:47:20 +0300

    modified it a little more

--- main.c
+++ main.c
@@ -77,13 +77,94 @@ void insert_sorted(Node** root, int value){
 
 }
 
+void remove_element(Node** root, int value){//only removes the first occurence of the element
+    if (*root == NULL){
+        return; // element not found
+    }
+    if((*root)->x == value){//edge cases
+        Node* to_remove = *root;
+        *root = (*root)->next;
+        free(to_remove);
+        return;
+    }
+    for(Node* curr = *root; curr->next!=NULL;curr=curr->next){
+        if(curr->next->x == value){
+            Node* to_remove = curr->next;
+            curr->next = curr->next->next;
+            free(to_remove);
+            return;
+        }
+    }
+}
+
+void reverse(Node** root){
+    Node* prev = NULL;
+    Node* curr = *root;
+
+    while(curr!=NULL){
+        Node* next = curr->next;
+
+        curr->next = prev;
+
+        prev = curr;
+        curr = next;
+
+    }
+    *root = prev;
+
+}
+
+int has_loops(Node* root){
+    Node* slow = root;
+    Node* fast = root;
+    while (slow != NULL && fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow==fast){//checking if pointing to the same address which means there is a cycle/loop
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int count(Node* root)
+{
+    int cnt = 0;
+    for (Node* curr = root; curr !=NULL;curr= curr->next){
+        cnt++;
+    }
+    return cnt;
+}
+
+int count_rec(Node* node){ // counting elements in recursive manner.
+    if (node == NULL){
+        return 0;
+    }
+    return 1 + count_rec(node->next);
+}
+
 int main() {
     Node* root = NULL;
-    insert_sorted(&root, 1);
-    insert_sorted(&root,2);
-    insert_sorted(&root,0);
-    insert_sorted(&root,-1);
+//    insert_sorted(&root, 1);
+//    insert_sorted(&root,2);
+//    insert_sorted(&root,0);
+//    insert_sorted(&root,-1);
+    insert_end(&root,1);
+    insert_end(&root,1);
+    insert_end(&root,3);
+    insert_end(&root,6);
+    insert_end(&root,7);
 
+//    root->next->next->next->next->next = root->next;
+
+    if(has_loops(root) == 1){
+        printf("linked list has a loop\n");
+        return 1;
+    }
+
+//    reverse(&root);
+
+//    remove_element(&root, 2);
 
 //    insert_beginning(&root, 13);
 //    insert_beginning(&root, 15);
@@ -96,9 +177,9 @@ int main() {
     for(Node* curr= root; curr !=NULL; curr= curr->next){
         printf("%d\n", curr->x);
     }
+
+//    printf("\nLinked list has %d elements\n", count(root));
+    printf("\nLinked list has %d elements\n", count_rec(root));
     deallocate(&root);
-
-
-
     return 0;
 }
